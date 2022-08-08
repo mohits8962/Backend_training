1fe3c8599af3ec62b7acf8079d0d186a93ae60bc
mohits8962 <108714331+mohits8962@users.noreply.github.com>
2022-08-08 20:19:00 +0530

    adding these file 8 aug assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,4 +35,87 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+// ASSIGNMENT--------------------------------------------------------------------------------------------------
+
+
+// problem 01
+router.get('/movies', function (req, res){
+    let getMovies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(getMovies)
+    res.send("problem 01 run successfully")
+})
+
+
+// problem 02
+// router.get('/movies/:indexNumber', function (req, res){
+//     let getMovies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+//     let reqParams = req.params
+//     let movieIndex = reqParams.indexNumber
+//     let movieName=getMovies[movieIndex]
+//     console.log(movieName)
+    
+
+//     res.send("problem 02 run successfully")
+// })
+
+// problem 03
+router.get('/movies/:indexNumber', function (req, res){
+    let getMovies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let reqParams = req.params
+    let movieIndex = reqParams.indexNumber
+    let movieName=getMovies[movieIndex]
+    console.log(movieName||"Error : use a valid index")
+    
+
+    res.send("problem 03 run successfully")
+})
+
+
+// problem 04
+router.get('/films', function (req, res){
+    let movieArray = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+        console.log(movieArray)
+
+    res.send("problem 04 run successfully")
+})
+
+
+// problem 05
+router.get('/films/:filmId', function (req, res){
+    let movieArray = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+
+        let reqParams = req.params
+        let movieId = reqParams.filmId
+        let filmArray = movieArray[movieId]
+        console.log(filmArray||"Error : use a valid index") 
+
+    res.send("problem 05 run successfully")
+})
+
 module.exports = router;

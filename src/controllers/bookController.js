6d79ebddb2bdb27cbba5de4de2aa8f6303f89681
mohits8962 <108714331+mohits8962@users.noreply.github.com>
2022-08-17 00:39:55 +0530

    adding this files

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,14 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
+// const { count } = require("console")
+// const BookModel= require("../models/bookModel")
 
-const createBook= async function (req, res) {
-    let data= req.body
+// const createBook= async function (req, res) {
+//     let data= req.body
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
-}
+//     let savedData= await BookModel.create(data)
+//     res.send({msg: savedData})
+// }
 
-const getBooksData= async function (req, res) {
+// const getBooksData= async function (req, res) {
 
     // let allBooks= await BookModel.find( ).count() // COUNT
 
@@ -65,21 +65,74 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+//     let a= 2+4
+//     a= a + 10
+//     console.log(a)
+//     let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
-    // WHEN AWAIT IS USED: - database + axios
-    //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
+//     // WHEN AWAIT IS USED: - database + axios
+//     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
+//     console.log(allBooks)
+//     let b = 14
+//     b= b+ 10
+//     console.log(b)
+//     res.send({msg: allBooks})
+// }
+
+
+// module.exports.createBook= createBook
+// module.exports.getBooksData= getBooksData
+
+
+
+
+
+// Assignment------------------------------------------------------------------------------------------------------------
+
+
+
+const BookModel= require("../models/bookModel")
+
+const createBook= async function (req, res) {
+    let data= req.body
+    let savedData= await BookModel.create(data)
+    res.send({msg: savedData})
+}
+
+const bookList= async function (req, res) {
+    let listBooks= await BookModel.find().select( { bookName : 1 , authorName: 1 } )
+    res.send({msg: listBooks})
+}
+
+const getBooksInYear= async function (req, res) {
+    let data= req.query.year
+    let yearOfBooks= await BookModel.find({year:{$eq: data}})
+    res.send({msg: yearOfBooks})
+}
+
+const getParticularBooks=async function(req,res){
+    let data=req.body;
+    let particularData=await BookModel.find(data);
+    res.send({msg:particularData});
 }
 
 
+const getXINRBooks=async function(req,res){
+    let INROfBooks=await BookModel.find({$or:[{"prices.indianPrice":"100INR"},{"prices.indianPrice":{$eq:"200INR"}},{"prices.indianPrice":{$eq:"500INR"}}]})
+        res.send({msg: INROfBooks})
+    }
+
+
+const getRandomBooks=async function(req,res){
+        let data=await BookModel.find({$or:[{stockAvailable:true},{totalPages:{$gt:500}}]})
+        res.send({msg:data})
+    }
+
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.bookList= bookList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,12 +8,30 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+// router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+// router.get("/getBooksData", BookController.getBooksData)
+
+// Assignment------------------------------------------------------------------------------------------------------------
+
+
+router.post("/createBook", BookController.createBook )
+
+router.get("/bookList", BookController.bookList)
+
+router.post("/getBooksInYear", BookController.getBooksInYear )
+
+router.post("/getParticularBooks", BookController.getParticularBooks )
+
+router.post("/getXINRBooks", BookController.getXINRBooks )
+
+router.post("/getRandomBooks", BookController.getRandomBooks )
+
+
+
 
 module.exports = router;

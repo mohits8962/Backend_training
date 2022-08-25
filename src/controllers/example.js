46a5ead29d65e6d73472794f89b967e2152119ac
mohits8962 <108714331+mohits8962@users.noreply.github.com>
2022-08-25 15:27:19 +0530

    add\

---
+++ src/controllers/example.js
@@ -0,0 +1,20 @@
+let obj ={
+    "firstName" : "mohit",
+    "lastName" : "sahu",
+    "city": "jabalpur",
+   "address":{
+    "pincode":"482020"
+   }
+}
+
+console.log(obj.firstName)
+console.log(obj["firstName"])
+
+console.log(obj.address.pincode)
+console.log(obj["address.pincode"])
+
+// obj.age=22
+// obj["age"]=22
+
+// console.log(obj)
+
